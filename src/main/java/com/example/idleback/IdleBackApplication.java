c7ce32b533dc542d2a95c0338af8cb94c8620e37
JaimeBenitez <jbenaci50@gmail.com>
2023-03-09 13:22:34 +0100

    commit inicial

---
+++ src/main/java/com/example/idleback/IdleBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.idleback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdleBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdleBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/idleback/IdleBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.idleback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdleBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
