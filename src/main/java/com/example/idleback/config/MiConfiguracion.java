b1d4d323a2813a1837967d48d32334cacc43aba7
JaimeBenitez <jbenaci50@gmail.com>
2023-04-06 17:06:49 +0200

    Arreglado cors para que permita todos los metodos

--- src/main/java/com/example/idleback/config/MiConfiguracion.java
+++ src/main/java/com/example/idleback/config/MiConfiguracion.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class MiConfiguracion {
@@ -17,7 +18,8 @@ public class MiConfiguracion {
         return new WebMvcConfigurer() {
         @Override
             public void addCorsMappings(CorsRegistry registry){
-            registry.addMapping("/**");
+                    registry.addMapping("/**")
+                    .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
         }
         };
     }
